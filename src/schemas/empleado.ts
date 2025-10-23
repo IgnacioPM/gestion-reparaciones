470692c7e24eeb9e74d319b940c8c822beb3e296
IgnacioPM <ignacio040890@gmail.com>
2025-10-23 04:50:40 -0600

    correción en modal y form empleado 9

--- src/schemas/empleado.ts
+++ src/schemas/empleado.ts
@@ -5,7 +5,7 @@ export const EmpleadoSchema = z.object({
   nombre: z.string().min(1),
   email: z.string().email(),
   password: z.string().min(6).optional(),
-  rol: z.enum(['Tecnico', 'Admin']).default('Tecnico'),
+  rol: z.enum(['Admin', 'Tecnico']).optional(),
 })
 
 export type EmpleadoFormData = z.infer<typeof EmpleadoSchema>
